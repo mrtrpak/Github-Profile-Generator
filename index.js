fa8c8b123ac7a4cba058ee62d996ad3d29ce0235
Michael Trpak <mrtrpak3@gmail.com>
2019-11-10 18:14:44 -0500

    added in inquirer prompts and is working properly

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-var axios = require("axios");
-var inquirer = require("inquirer");
+const axios = require("axios");
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+
+
+inquirer.prompt([
+    {
+        type: "input",
+        message: "What is your Github username?",
+        name: "username"
+    },
+    {
+        type: "list",
+        message: "What is your favorite color?",
+        choices: ["Blue", "Green", "Orange", "Purple", "Red", "White", "Yellow"],
+        default: 3,
+        name: "favColor"
+    }
+])
