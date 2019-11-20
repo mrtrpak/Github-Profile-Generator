e11dbfde42696e83019cba507fa98c9dc0029ae1
Michael Trpak <mrtrpak3@gmail.com>
2019-11-19 21:33:36 -0500

    made my writeFile into a template literal

--- index.js
+++ index.js
@@ -24,15 +24,17 @@ inquirer.prompt([
     const queryUrl = 'https://api.github.com/users/' + username;
     axios.get(queryUrl).then(gitInfo => {
         fs.writeFile(username + ".md",
-        "<h1 style='color:" + favColor + "'>" + gitInfo.data.name + "</h1>"
-        + "<img src=" + gitInfo.data.avatar_url + " style='width: 275px; height: 275px;'>"
-        + "<p>Bio: " + gitInfo.data.bio + "</p>"
-        + "<p>Company: " + gitInfo.data.company + "</p>"
-        + "<a href='" + gitInfo.data.html_url + "'>" + gitInfo.data.name + "'s Github Homepage</a>"
-        + "<p>Public Repos: " + gitInfo.data.public_repos + "</p>"
-        + "<p>Followers: " + gitInfo.data.followers + "</p>"
-        + "<p>Following: " + gitInfo.data.following + "</p>"
-        + "<p>Location: " + gitInfo.data.location + "</p>",
+
+`<h1 style="color:${favColor}">${gitInfo.data.name}</h1>
+<img src="${gitInfo.data.avatar_url}" style="width: 275px; heigh: 275px;">
+<p>Bio: ${gitInfo.data.bio}</p>
+<p>Company: ${gitInfo.data.company}</p>
+<a href="${gitInfo.data.html_url}">${gitInfo.data.name}'s Github Homepage</a>
+<p>Public Repos: ${gitInfo.data.public_repos}</p>
+<p>Followers: ${gitInfo.data.followers}</p>
+<p>Following: ${gitInfo.data.following}</p>
+<p>Location: ${gitInfo.data.location}</p>`,
+
         error => {
             if (error) {
                 return error;
--- mrtrpak.md
+++ mrtrpak.md
@@ -1,1 +1,9 @@
-<h1 style='color:green'>Michael Trpak</h1><img src=https://avatars2.githubusercontent.com/u/54956694?v=4 style='width: 275px; height: 275px;'><p>Bio: I am an inspiring web developer who is looking to be challenged and fulfilled in an exciting career field.</p><p>Company: Student at UNCC's Full Stack Web Development Program</p><a href='https://github.com/mrtrpak'>Michael Trpak's Github Homepage</a><p>Public Repos: 17</p><p>Followers: 0</p><p>Following: 0</p><p>Location: Charlotte, North Carolina</p>
+<h1 style="color:orange">Michael Trpak</h1>
+<img src="https://avatars2.githubusercontent.com/u/54956694?v=4" style="width: 275px; heigh: 275px;">
+<p>Bio: I am an inspiring web developer who is looking to be challenged and fulfilled in an exciting career field.</p>
+<p>Company: Student at UNCC's Full Stack Web Development Program</p>
+<a href="https://github.com/mrtrpak">Michael Trpak's Github Homepage</a>
+<p>Public Repos: 19</p>
+<p>Followers: 0</p>
+<p>Following: 1</p>
+<p>Location: Charlotte, North Carolina</p>
